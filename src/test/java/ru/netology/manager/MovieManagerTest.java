a33f74adb686bf91eaedf5ad0ffe2c3ddf805ceb
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-02-06 21:14:40 +0300

    initial commit

--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -9,19 +9,19 @@ class MovieManagerTest {
 
     @Test
     void shouldOutput10LastAddMoviesFromAllList() {
-        MovieManager manager = new MovieManager();
-        MovieItem first = new MovieItem("1", "https://1", "bladshot", "ActionMovie");
-        MovieItem second = new MovieItem("2", "https://2", "vpered", "Cartoon");
-        MovieItem third = new MovieItem("3", "https://3", "belgrad", "Comedy");
-        MovieItem fourth = new MovieItem("4", "https://4", "dgentelmens", "ActionMovie");
-        MovieItem fifth = new MovieItem("5", "https://5", "men-inbisible", "Horror");
-        MovieItem sixth = new MovieItem("6", "https://6", "trolly", "Cartoon");
-        MovieItem seventh = new MovieItem("7", "https://7", "numberOne", "Comedy");
-        MovieItem eighth = new MovieItem("8", "https://8", "Strelcov", "ActionMovie");
-        MovieItem ninth = new MovieItem("9", "https://9", "describle-me", "Cartoon");
-        MovieItem tenth = new MovieItem("10", "https://10", "vampire", "Horror");
-        MovieItem eleventh = new MovieItem("11", "https://11", "sharks", "Horror");
-        MovieItem twelve = new MovieItem("12", "https://12", "Shadow", "Horror");
+        MovieManager manager= new MovieManager();
+        MovieItem first = new MovieItem("1236547", "https://1236547", "Bladshot", "Action");
+        MovieItem second = new MovieItem("2574987", "https://2574987", "Vpered", "Cartoon");
+        MovieItem third = new MovieItem("12547895", "https://12547895", "Belgrad", "Comedy");
+        MovieItem fourth = new MovieItem("2547952", "https://2547952", "Gentleman", "Action");
+        MovieItem fifth = new MovieItem("3579845", "https://3579845", "men-inbisible", "Horror");
+        MovieItem sixth = new MovieItem("9872547", "https://9872547", "Trolly", "Cartoon");
+        MovieItem seventh = new MovieItem("6970235", "https://6970235", "Number One", "Comedy");
+        MovieItem eighth = new MovieItem("5710025", "https://5710025", "Sex on the city", "Drama");
+        MovieItem ninth = new MovieItem("9813574", "https://9813574", "Legend 17", "Drama");
+        MovieItem tenth = new MovieItem("1012579", "https://1012579", "Speakerphone", "Comedy");
+        MovieItem eleventh = new MovieItem("9870230", "https://9870230", "Soul", "Cartoon");
+        MovieItem twelve = new MovieItem("4225478", "https://4225478", "Fire", "Drama");
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -42,11 +42,11 @@ class MovieManagerTest {
     @Test
     void shouldOutputLastAddMoviesFromAllListIfTheirLess10() {
         MovieManager manager = new MovieManager();
-        MovieItem first = new MovieItem("1", "https://1", "bladshot", "ActionMovie");
-        MovieItem second = new MovieItem("2", "https://2", "vpered", "Cartoon");
-        MovieItem third = new MovieItem("3", "https://3", "belgrad", "Comedy");
-        MovieItem fourth = new MovieItem("4", "https://4", "dgentelmens", "ActionMovie");
-        MovieItem fifth = new MovieItem("5", "https://5", "men-inbisible", "Horror");
+        MovieItem first = new MovieItem("1236547", "https://1236547", "Bladshot", "Action");
+        MovieItem second = new MovieItem("2574987", "https://2574987", "Vpered", "Cartoon");
+        MovieItem third = new MovieItem("12547895", "https://12547895", "Belgrad", "Comedy");
+        MovieItem fourth = new MovieItem("2547952", "https://2547952", "Gentleman", "Action");
+        MovieItem fifth = new MovieItem("3579845", "https://3579845", "men-inbisible", "Horror");
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -60,17 +60,17 @@ class MovieManagerTest {
     @Test
     void shouldOut5MoviesFromAllList() {
         MovieManager manager = new MovieManager(5);
-        MovieItem first = new MovieItem("1", "https://1", "bladshot", "ActionMovie");
-        MovieItem second = new MovieItem("2", "https://2", "vpered", "Cartoon");
-        MovieItem third = new MovieItem("3", "https://3", "belgrad", "Comedy");
-        MovieItem fourth = new MovieItem("4", "https://4", "dgentelmens", "ActionMovie");
-        MovieItem fifth = new MovieItem("5", "https://5", "men-inbisible", "Horror");
-        MovieItem sixth = new MovieItem("6", "https://6", "trolly", "Cartoon");
-        MovieItem seventh = new MovieItem("7", "https://7", "numberOne", "Comedy");
-        MovieItem eighth = new MovieItem("8", "https://8", "Strelcov", "ActionMovie");
-        MovieItem ninth = new MovieItem("9", "https://9", "describle-me", "Cartoon");
-        MovieItem tenth = new MovieItem("10", "https://10", "vampire", "Horror");
-        MovieItem eleventh = new MovieItem("11", "https://11", "sharks", "Horror");
+        MovieItem first = new MovieItem("1236547", "https://1236547", "Bladshot", "Action");
+        MovieItem second = new MovieItem("2574987", "https://2574987", "Vpered", "Cartoon");
+        MovieItem third = new MovieItem("12547895", "https://12547895", "Belgrad", "Comedy");
+        MovieItem fourth = new MovieItem("2547952", "https://2547952", "Gentleman", "Action");
+        MovieItem fifth = new MovieItem("3579845", "https://3579845", "men-inbisible", "Horror");
+        MovieItem sixth = new MovieItem("9872547", "https://9872547", "Trolly", "Cartoon");
+        MovieItem seventh = new MovieItem("6970235", "https://6970235", "Number One", "Comedy");
+        MovieItem eighth = new MovieItem("5710025", "https://5710025", "Sex on the city", "Drama");
+        MovieItem ninth = new MovieItem("9813574", "https://9813574", "Legend 17", "Drama");
+        MovieItem tenth = new MovieItem("1012579", "https://1012579", "Speakerphone", "Comedy");
+        MovieItem eleventh = new MovieItem("9870230", "https://9870230", "Soul", "Cartoon");
         manager.add(first);
         manager.add(second);
         manager.add(third);
