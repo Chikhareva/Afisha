d1a3fffce846ad1a7620b4f42d1ae6141b295434
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-02-08 23:35:50 +0300

    refactor code

--- src/main/java/ru/netology/domain/PurchaseItem.java
+++ src/main/java/ru/netology/domain/PurchaseItem.java
@@ -4,14 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
-public class PurchaseItem {
-    private int id;
-    private int productId;
-    private String productName;
-    private int productPrice;
-    private int count;
-}
+
+    public class PurchaseItem {
+        private int id;
+        private int productId;
+        private String productName;
+        private int productPrice;
+        private int count;
+    }
 
